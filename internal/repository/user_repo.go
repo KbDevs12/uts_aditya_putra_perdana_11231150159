f10bfc2d168a3f66d316b091d4ad7293496e6543
KbDevs12 <daniaku203@gmail.com>
2026-04-22 15:48:52 +0700

    membuat repository user

---
+++ internal/repository/user_repo.go
@@ -0,0 +1,28 @@
+package repository
+
+import (
+	"backend/internal/domain"
+
+	"gorm.io/gorm"
+)
+
+type UserRepo struct {
+	db *gorm.DB
+}
+
+func NewUserRepo(db *gorm.DB) *UserRepo {
+	return &UserRepo{db}
+}
+
+func (r *UserRepo) FindByUID(uid string) (*domain.User, error) {
+	var user domain.User
+	err := r.db.Where("firebase_uid = ?", uid).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepo) Create(user *domain.User) error {
+	return r.db.Create(user).Error
+}
