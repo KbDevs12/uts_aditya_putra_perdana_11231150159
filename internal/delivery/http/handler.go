6699c88ee6760e4894f73433046ef5b7938be29c
KbDevs12 <daniaku203@gmail.com>
2026-04-22 17:17:34 +0700

    perbarui handler

--- internal/delivery/http/handler.go
+++ internal/delivery/http/handler.go
@@ -1,10 +1,21 @@
 package http
 
-import "backend/internal/usecase"
+import (
+	"backend/internal/usecase"
+)
 
 type Handler struct {
 	authUC    *usecase.AuthUsecase
 	productUC *usecase.ProductUsecase
 	cartUC    *usecase.CartUsecase
 	orderUC   *usecase.OrderUsecase
+}
+
+func NewHandler(
+	authUC *usecase.AuthUsecase,
+	productUC *usecase.ProductUsecase,
+	cartUC *usecase.CartUsecase,
+	orderUC *usecase.OrderUsecase,
+) *Handler {
+	return &Handler{authUC, productUC, cartUC, orderUC}
 }
