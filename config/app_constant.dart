b3c6ff4fb9db61b6b372facfbb374d5bfc521c8e
KbDevs12 <daniaku203@gmail.com>
2026-04-22 20:33:51 +0700

    setup constant untuk aplikasi

---
+++ config/app_constant.dart
@@ -0,0 +1,10 @@
+class AppConstant {
+  static const String baseUrl = "nanti disini";
+
+  static const String register = '/auth/register';
+  static const String login = '/auth/login';
+  static const String products = '/api/products';
+  static const String cart = '/api/cart';
+  static const String checkout = '/api/orders/checkout';
+  static const String orders = '/api/orders';
+}
