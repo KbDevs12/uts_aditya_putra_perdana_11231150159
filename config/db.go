982ec43d46a83f96c5d8b2f11c8896b2829d5ac0
KbDevs12 <daniaku203@gmail.com>
2026-06-30 18:05:12 +0700

    feat(backend):update db config

--- config/db.go
+++ config/db.go
@@ -1,7 +1,10 @@
 package config
 
 import (
+	"log"
 	"os"
+
+	"backend/internal/domain"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -9,9 +12,28 @@ import (
 
 func ConnectDB() *gorm.DB {
 	dsn := os.Getenv("DB_DSN")
+	if dsn == "" {
+		log.Fatal("DB_DSN is empty. Set DB_DSN in .env")
+	}
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
+
+	if err := db.AutoMigrate(
+		&domain.User{},
+		&domain.Product{},
+		&domain.Cart{},
+		&domain.CartItem{},
+		&domain.Order{},
+		&domain.OrderItem{},
+		&domain.WalletAccount{},
+		&domain.WalletTransaction{},
+		&domain.PaymentIntent{},
+	); err != nil {
+		log.Fatal("failed to migrate database: ", err)
+	}
+
 	return db
 }
