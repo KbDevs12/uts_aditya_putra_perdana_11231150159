1379b2017e255ca80e6d32b19bd731933cb2e1a1
KbDevs12 <daniaku203@gmail.com>
2026-06-30 16:59:58 +0700

    feat(backend):update order repo

--- internal/repository/order_repo.go
+++ internal/repository/order_repo.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"time"
+
 	"backend/internal/domain"
 
 	"gorm.io/gorm"
@@ -28,6 +30,23 @@ func (r *OrderRepo) GetByID(id int64) (*domain.Order, error) {
 	var order domain.Order
 	err := r.db.First(&order, id).Error
 	return &order, err
+}
+
+func (r *OrderRepo) AttachPaymentIntent(orderID int64, token string) error {
+	return r.db.Model(&domain.Order{}).Where("id = ?", orderID).Updates(map[string]interface{}{
+		"payment_intent_token": token,
+		"payment_status":       "unpaid",
+		"payment_method":       "global_wallet",
+	}).Error
+}
+
+func (r *OrderRepo) MarkPaid(tx *gorm.DB, orderID int64, paymentMethod string, paidAt time.Time) error {
+	return tx.Model(&domain.Order{}).Where("id = ?", orderID).Updates(map[string]interface{}{
+		"status":         "paid",
+		"payment_status": "paid",
+		"payment_method": paymentMethod,
+		"paid_at":        paidAt,
+	}).Error
 }
 
 type OrderItemRepo struct {
